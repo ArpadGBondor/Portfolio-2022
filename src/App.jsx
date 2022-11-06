232c5e93ab9159a113c6a015dd39738421f1e719
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2022-11-06 14:02:38 +0000

    Use toast notifications to show error messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import { Container, Box } from '@mui/material';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { SwitchTransition, CSSTransition } from 'react-transition-group';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { usePagesContext } from './context/pages/pagesContext';
 
 import Navbar from './components/Navbar';
@@ -11,40 +14,52 @@ import Page from './components/Page';
 function App() {
     const location = useLocation();
     const nodeRef = useRef(null);
-    const { index, cv, introduction, projects } = usePagesContext();
+    const { index, cv, introduction, projects, error, clearError } = usePagesContext();
+    useEffect(() => {
+        if (error) {
+            toast.error(error);
+            clearError();
+        }
+    }, [error]);
     return (
-        <Container
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                minHeight: '100vh',
-            }}
-        >
-            <Navbar />
-            <SwitchTransition mode="out-in">
-                <CSSTransition
-                    key={location.key}
-                    timeout={500}
-                    classNames="page-load"
-                    nodeRef={nodeRef}
-                    addEndListener={(done) => {
-                        nodeRef.current.addEventListener('transitionend', done, false);
-                    }}
-                >
-                    <Box sx={{ width: '100%' }} ref={nodeRef}>
-                        <Routes location={location}>
-                            <Route path="/" element={<Page page={index} pageId="index" />} />
-                            <Route path="/cv" element={<Page page={cv} pageId="cv" />} />
-                            <Route path="/introduction" element={<Page page={introduction} pageId="introduction" />} />
-                            <Route path="/projects" element={<Page page={projects} pageId="projects" />} />
-                        </Routes>
-                    </Box>
-                </CSSTransition>
-            </SwitchTransition>
-            <Footer />
-        </Container>
+        <>
+            <Container
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    minHeight: '100vh',
+                }}
+            >
+                <Navbar />
+                <SwitchTransition mode="out-in">
+                    <CSSTransition
+                        key={location.key}
+                        timeout={500}
+                        classNames="page-load"
+                        nodeRef={nodeRef}
+                        addEndListener={(done) => {
+                            nodeRef.current.addEventListener('transitionend', done, false);
+                        }}
+                    >
+                        <Box sx={{ width: '100%' }} ref={nodeRef}>
+                            <Routes location={location}>
+                                <Route path="/" element={<Page page={index} pageId="index" />} />
+                                <Route path="/cv" element={<Page page={cv} pageId="cv" />} />
+                                <Route
+                                    path="/introduction"
+                                    element={<Page page={introduction} pageId="introduction" />}
+                                />
+                                <Route path="/projects" element={<Page page={projects} pageId="projects" />} />
+                            </Routes>
+                        </Box>
+                    </CSSTransition>
+                </SwitchTransition>
+                <Footer />
+            </Container>
+            <ToastContainer />
+        </>
     );
 }
 
