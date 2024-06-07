66b3b599b699e187ed89483243eeedec4ca606cf
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2024-06-07 01:13:06 +0100

    fix: show project years

--- src/components/paragraphs/Paragraph.jsx
+++ src/components/paragraphs/Paragraph.jsx
@@ -6,52 +6,48 @@ import { Typography, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 function Paragraph({ p }) {
-    const navigate = useNavigate();
-    if (typeof p !== 'object') return <Typography>Paragraph not implemented.</Typography>;
-    switch (p.type) {
-        case 'email':
-            return <Message />;
-        case 'contacts':
-            return <Contacts />;
-        case 'typing':
-            return <Typing sentences={p.sentences} />;
-        case 'text':
-            return (
-                <Typography sx={{ width: '100%', textAlign: 'left' }} variant="p">
-                    {p.text}
-                </Typography>
-            );
-        case 'link':
-            if (p.href.includes('http')) {
-                return (
-                    <a href={p.href} style={{ textDecoration: 'none' }} target="_blank" rel="noreferrer">
-                        <Button sx={{ margin: '1rem auto', display: 'block' }} variant="contained">
-                            {p.text}
-                        </Button>
-                    </a>
-                );
-            } else {
-                return (
-                    <Button
-                        sx={{ margin: '1rem auto', display: 'block' }}
-                        variant="contained"
-                        onClick={() => navigate(p.href)}
-                    >
-                        {p.text}
-                    </Button>
-                );
-            }
-        case 'image':
-            const style = { margin: '1rem' };
-            if (p.width) style.width = p.width;
-            if (p.height) style.height = p.height;
-            if (p.maxHeight) style.maxHeight = p.maxHeight;
-            if (p.maxWidth) style.maxWidth = p.maxWidth;
-            if (p.style === 'circular') style.borderRadius = '50%';
-            return <img style={style} src={p.src} alt={p.alt} />;
-        default:
-            return <Typography variant="p">Paragraph not implemented.</Typography>;
-    }
+  const navigate = useNavigate();
+  if (typeof p !== 'object') return <Typography>Paragraph not implemented.</Typography>;
+  switch (p.type) {
+    case 'email':
+      return <Message />;
+    case 'contacts':
+      return <Contacts />;
+    case 'typing':
+      return <Typing sentences={p.sentences} />;
+    case 'text':
+      return (
+        <Typography sx={{ display: 'block', width: '100%', textAlign: 'justify', marginBottom: '0.5rem' }} variant="p">
+          {p.text}
+        </Typography>
+      );
+    case 'link':
+      if (p.href.includes('http')) {
+        return (
+          <a href={p.href} style={{ textDecoration: 'none' }} target="_blank" rel="noreferrer">
+            <Button sx={{ margin: '1rem auto', display: 'block' }} variant="contained">
+              {p.text}
+            </Button>
+          </a>
+        );
+      } else {
+        return (
+          <Button sx={{ margin: '1rem auto', display: 'block' }} variant="contained" onClick={() => navigate(p.href)}>
+            {p.text}
+          </Button>
+        );
+      }
+    case 'image':
+      const style = { margin: '1rem' };
+      if (p.width) style.width = p.width;
+      if (p.height) style.height = p.height;
+      if (p.maxHeight) style.maxHeight = p.maxHeight;
+      if (p.maxWidth) style.maxWidth = p.maxWidth;
+      if (p.style === 'circular') style.borderRadius = '50%';
+      return <img style={style} src={p.src} alt={p.alt} />;
+    default:
+      return <Typography variant="p">Paragraph not implemented.</Typography>;
+  }
 }
 
 export default Paragraph;
--- src/components/sections/Section.jsx
+++ src/components/sections/Section.jsx
@@ -4,72 +4,74 @@ import { useThemeContext } from '../../context/theme/themeContext';
 import Paragraph from '../paragraphs/Paragraph';
 
 function Section({ section }) {
-    const { theme } = useThemeContext();
+  const { theme } = useThemeContext();
 
-    return (
-        <section>
-            <Box
+  return (
+    <section>
+      <Box
+        sx={{
+          minHeight: '50vh',
+          background: `url(${section.background_img_url}) center/cover fixed no-repeat, #7777`,
+          position: 'relative',
+          marginBottom: '1rem',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          height: '100%',
+        }}
+      >
+        <Box
+          sx={{
+            backgroundColor: theme === 'light' ? '#BBDEFBDD' : '#212121DD',
+            position: 'absolute',
+            top: '0',
+            left: '0',
+            width: '100%',
+            height: '100%',
+          }}
+        ></Box>
+        <Container
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '100%',
+            height: '100%',
+            textAlign: 'justify',
+            padding: '1rem',
+            zIndex: '1',
+          }}
+        >
+          <Box sx={{ margin: '1rem', textAlign: 'center' }}>
+            <Typography variant="h2">{section.h2}</Typography>
+            {section.small && <small>{section.small}</small>}
+          </Box>
+
+          <Box sx={{ width: '100%' }}>
+            {section.img_src && (
+              <Box
                 sx={{
-                    minHeight: '50vh',
-                    background: `url(${section.background_img_url}) center/cover fixed no-repeat, #7777`,
-                    position: 'relative',
-                    marginBottom: '1rem',
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    width: '100%',
-                    height: '100%',
+                  margin: 'auto',
+                  float: { sm: 'none', md: 'right' },
+                  width: { sm: '75%', md: '50%' },
+                  padding: { sm: '2rem', md: '0 0 0 1rem' },
                 }}
-            >
-                <Box
-                    sx={{
-                        backgroundColor: theme === 'light' ? '#BBDEFBDD' : '#212121DD',
-                        position: 'absolute',
-                        top: '0',
-                        left: '0',
-                        width: '100%',
-                        height: '100%',
-                    }}
-                ></Box>
-                <Container
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        width: '100%',
-                        height: '100%',
-                        textAlign: 'justify',
-                        padding: '1rem',
-                        zIndex: '1',
-                    }}
-                >
-                    <Typography sx={{ margin: '1rem', textAlign: 'center' }} variant="h2">
-                        {section.h2}
-                    </Typography>
-                    <Box sx={{ width: '100%' }}>
-                        {section.img_src && (
-                            <Box
-                                sx={{
-                                    margin: 'auto',
-                                    float: { sm: 'none', md: 'right' },
-                                    width: { sm: '75%', md: '50%' },
-                                    padding: { sm: '2rem', md: '0 0 0 1rem' },
-                                }}
-                            >
-                                <img style={{ width: '100%' }} src={section.img_src} alt={`${section.h2} section`} />
-                            </Box>
-                        )}
+              >
+                <img style={{ width: '100%' }} src={section.img_src} alt={`${section.h2} section`} />
+              </Box>
+            )}
 
-                        {section.p.map((p, idx) => (
-                            <Paragraph key={idx} p={p} />
-                        ))}
-                    </Box>
-                </Container>
-            </Box>
-        </section>
-    );
+            {section.p.map((p, idx) => (
+              <Paragraph key={idx} p={p} />
+            ))}
+          </Box>
+        </Container>
+      </Box>
+    </section>
+  );
 }
 
 export default Section;
