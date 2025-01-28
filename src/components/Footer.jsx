dd3612c770ad30148c7195cb5b21118487002155
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2025-01-28 02:07:54 +0000

    fix: update year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 import { Container, Typography } from '@mui/material';
 
 function Footer() {
-    return (
-        <>
-            <Container sx={{ height: '1rem' }} />
-            <Container
-                sx={{
-                    position: 'absolute',
-                    bottom: '0',
-                    width: '100%',
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-                }}
-            >
-                <Typography>Copyright © 2022-2024 Árpád Gábor Bondor</Typography>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <Container sx={{ height: '1rem' }} />
+      <Container
+        sx={{
+          position: 'absolute',
+          bottom: '0',
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography>Copyright © 2022-2025 Árpád Gábor Bondor</Typography>
+      </Container>
+    </>
+  );
 }
 
 export default Footer;
